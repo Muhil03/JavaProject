8155f6d75b97e1c080da5e0df10d69c5179dcbe3
Muhil03 <muhilarasan223@gmail.com>
2022-11-23 20:06:55 +0530

    Java

---
+++ src/Day2/Tasks.java
@@ -0,0 +1,28 @@
+package Day2;
+
+public class Tasks {
+
+public static void main(String[] args) {
+		
+		//1st Task print
+		System.out.println("Hello");
+		System.out.println("Muhilarasan I");
+		
+		//2nd Task sum
+		int a=74;
+		int b=36;
+		
+		System.out.println(a+b);
+		
+		//3rd Task swap
+		
+		int c=a;
+		a=b;
+		b=c;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+}
